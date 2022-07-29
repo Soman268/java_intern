b185373edb90923df190234b008ca19a9a692855
Soman Kumar <54895319+Soman268@users.noreply.github.com>
2022-07-29 16:58:16 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,12 @@
+// using user input
+
+
+import java.util.*;
+
+public class string2 {
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      String name = sc.next();
+      System.out.println(name);
+    }
+}
