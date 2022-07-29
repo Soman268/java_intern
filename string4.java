b11ab03a62fbf3fd9226e10c8317bf0031c29c3d
Soman Kumar <54895319+Soman268@users.noreply.github.com>
2022-07-29 17:00:30 +0530

    Add files via upload

---
+++ string4.java
@@ -0,0 +1,14 @@
+//string length
+
+import java.util.*;
+
+public class string4 {
+    public static void main(String args[])
+    {
+        String first_name="soman";
+        String last_name="kumar";
+        String full_name = first_name + last_name ;
+        System.out.println(full_name.length());
+    }
+    
+}
---
+++ string5.java
@@ -0,0 +1,13 @@
+// using charAt method
+
+import java.util.*;
+
+public class string5 {
+    public static void main(String args[]){
+        String name="soman";
+        for(int i=0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+    
+}
