95a7b7c5a3dfb1304544948a83044a9218880ae2
Soman Kumar <54895319+Soman268@users.noreply.github.com>
2022-07-29 16:58:42 +0530

    Add files via upload

---
+++ string3.java
@@ -0,0 +1,14 @@
+//concatination
+
+import java.util.*;
+
+public class string3 {
+    public static void main(String args[])
+    {
+        String first_name="soman";
+        String last_name="kumar";
+        String full_name = first_name + last_name ;
+        System.out.println(full_name);
+    }
+    
+}
