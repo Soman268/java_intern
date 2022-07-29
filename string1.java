788d26b75cf9817f1823494679e065201511b481
Soman Kumar <54895319+Soman268@users.noreply.github.com>
2022-07-29 16:57:57 +0530

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,15 @@
+// declaration of string
+
+public class string1 {
+    public static void main(String args[]){
+        String name="soman";
+        String full_name="Soman Kumar";
+        String sentence = "hello my name is soman kumar:";
+        System.out.println(name);
+
+        System.out.println(full_name);
+
+        System.out.println(sentence);
+    }
+    
+}
