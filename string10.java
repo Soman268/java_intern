83754c1528e537e4ba67d91bc04b083c0cdd8a79
Soman Kumar <54895319+Soman268@users.noreply.github.com>
2022-07-30 13:49:37 +0530

    Add files via upload

---
+++ string10.java
@@ -0,0 +1,12 @@
+// string builder set char at index 0
+
+import java.util.*;
+
+public class string10 {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("soman");
+        sb.setCharAt(0, 'n');
+        System.out.println(sb);
+    }
+    
+}
---
+++ string9.java
@@ -0,0 +1,11 @@
+ // stringbuilder using char at index 0
+
+ import java.util.*;
+
+public class string9 {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("soman");
+        System.out.println(sb.charAt(0));
+    }
+    
+}
